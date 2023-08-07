bb34f6e2633bc49a3819085c5aa86ae7a1ef6ff2
forhad <forhadhossain0619@gmail.com>
2023-08-08 02:33:54 +0600

    change image directory

--- dist/js/team/team.js
+++ dist/js/team/team.js
@@ -3,30 +3,30 @@ const Member = [
         id:1,
         name :'Bodiuzzaman Emran',
         job :'Motion Graphics Expert',
-        image:'./images/emran.jpg'
+        image:'../images/emran.jpg'
     },
     {
         id:2,
         name:'Md. Farhad Hossain',
         job :'Graphics Designer',
-        image:'./images/forhad-mama.png'
+        image:'../images/forhad-mama.png'
     },
     {
         id:3,
         name:'Md. Anwar Hossen ',
         job:'Wordpress Developer',
-        image:'./images/anwar.png'
+        image:'../images/anwar.png'
     },
     {
         id:4,
         name:"Md Forhad Hossen",
         job:'Front-End Developer',
-        image:'./images/forhad.jpg'
+        image:'../images/forhad.jpg'
     },
     {
         id:5,
         name:"Md. Firoz Hossain",
         job:'Graphics Designer',
-        image:'/dist/images/fiz.png'
+        image:'../images/fiz.png'
     }
 ]
